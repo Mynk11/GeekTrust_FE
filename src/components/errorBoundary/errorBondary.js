56a350d89ffd0ef6f540ec99627f3c78422b99e5
{mayank} <{mayankguptamg.11@gmail.com}>
2019-09-06 00:20:48 +0530

    Errro boundry

---
+++ src/components/errorBoundary/errorBondary.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+class ErrorBoundary extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hasError: false
+        }
+    }
+    static getDerivedStateFromError(error) {
+
+
+        return {
+            hasError: true
+        }
+    }
+    render() {
+        if (this.state.hasError) {
+            return <h2 className="text-center pt-5">Try Again please</h2>
+        }
+        return this.props.children
+    }
+}
+export default ErrorBoundary
